7a6064457d4be15865c9f17a7db55dda6a9d3f55
MOHD SAOUD <mohdhashimansari30@gmail.com>
2025-06-05 12:15:19 +0530

    to do better

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Resume from './pages/Resume';
 import Footer from './components/Footer';
 import ThemeSwitcher from './components/ThemeSwitcher';
